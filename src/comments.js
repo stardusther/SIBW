4e9ffb1a871a7de9e0579d2032ea2397cb5eb0fc
Esther García <esther.g.g.01@gmail.com>
2022-03-25 09:49:05 +0100

    Completada clase comments.js
    Falta (si quiero) añadir los likes

--- src/comments.js
+++ src/comments.js
@@ -1,46 +1,60 @@
-// Mostrar/Ocultar comentarios anteriores
+
+// Variables ----------------------------------------------------------
+
 let showButton = document.getElementById("toggleComments"); // Botón que muestra los comentarios anteriores
+let commentSection = document.getElementById("commentSection"); // Sección de comentarios (comentarios anteriores)
+let formDiv = document.getElementById("flex-comentarios");
+let formulario = document.getElementById("form"); // El formulario de los datos del usuario
+let sendButton = document.getElementById('commentButton'); // Botón que publica el comentario
+let likeButton = document.getElementById('animate__heartBeat'); // Botón de corazón
 
-showButton.addEventListener("click", showComments); // FIXME no funciona sin el onclick dentro del html
 
-function showComments() {
-   let prevComments = document.getElementById("commentSection");
-   let formulario = document.getElementById("form");
+// Inicializar ----------------------------------------------------------
 
-   if (prevComments.style.display === "none") {
-        prevComments.style.display = "block";
-       formulario.style.display = "block";
-        showButton.innerText= "Ocultar comentarios";//fixme no funciona
-   } else {
-        prevComments.style.display = "none";
-        formulario.style.display = "none";
-        showButton.innerText = "Mostrar comentarios";
-   }
-    //alert("Hello! I am an alert box!");
+window.onload = init;
+
+function init() {
+    // Mostrar/Ocultar comentarios anteriores
+    showButton.addEventListener("click", showComments);
+    // Comprobar palabras que escribe el usuario
+    formulario.addEventListener("keyup", censor(document.getElementById('fcomment')));
+    formulario.addEventListener("keyup", censor(document.getElementById('fname')));
+    // Verificar y publicar comentario
+    formulario.addEventListener("submit", verificar);
+    likeButton.addEventListener("click", liked);
 }
 
-// Palabras prohibidas
-let commentbox = document.getElementById('comment');
-commentbox.addEventListener("keypress", checkWords);
+// liked
 
-function checkWords() {
-    let banned = ['coño','puta','gilipollas','cabrón'];
-    //var my_textarea = document.getElementById('textarea').value;
-    var pattern = /coño|puta|gilipollas|cabrón/ig;
+function liked(){
+    likebutton.toggleClass();
+}
 
-    /*if (commentbox.match(pattern)) {
-        commentbox = commentbox.replace(pattern, "****" );
-        document.getElementById('comment').value = commentbox;
-    }*/
+// Mostrar comentarios --------------------------------------------------
 
-    if (banned.some(word => commentbox.value.includes(word))) {
-        commentbox = commentbox.replace(pattern, "****" );
-        document.getElementById('comment').value = commentbox;
+function showComments() {
+    if (formDiv.style.display === "none") {
+        formDiv.style.display = "flex";
+        showButton.textContent = "Ocultar comentarios";
+    } else {
+        formDiv.style.display = "none";
+        showButton.textContent = "Mostrar comentarios";
+    }
+}
+
+// Censurar palabras prohibidas --------------------------------------------------
+
+function censor (palabra){
+    //var palabra = document.getElementById('fcomment');
+    let banned = ['coño', 'puta', 'cabrón', 'polla', 'joder', 'imbécil']; 
+    for(var aux of banned){
+        palabra.value = palabra.value.replace(aux, "*".repeat(aux.length));
     }
 }
 
 // Verificar comentario --------------------------------------------
-function verificar(){
+
+function verificar() {
     // Que los campos no están vacios
     let nom = document.forms["form"]["fname"].value;
     let mail = document.forms["form"]["femail"].value;
@@ -52,62 +66,49 @@ function verificar(){
     }
 
     // Validar email
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail) == false){
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail) == false) {
         alert("Has escrito una dirección de correo inválida");
+        
         return (false);
     }
 
-    publicarComentario(); // Procedemos a publicar tras todas las comprobaciones
-
+    publicarComentario();
 
 }
+
 
 // Publicar comentario formateado -----------------------------------
 
-const form = document.getElementById("form");
-const commentSection = document.getElementById("commentSection");
-//const commentContainer = document.getElementById('commentSection');
-
-form.addEventListener("submit", publicarComentario);
-
-function publicarComentario(){
-
+function publicarComentario() {
     // Guardamos la fecha actual dd/mm/yy hh:mm
-    let hoy = new Date ();
-    let fecha = " " + hoy.toLocaleString();
+    let hoy = new Date();
+    
+    let fechaDiv = document.createElement('span');
+    fechaDiv.className = "fecha";
+    fechaDiv.innerHTML = " " + hoy.toLocaleString();
 
     // Creamos el comentario
     const wrapper = document.createElement('div');
-    const nombre = document.forms["form"]["name"].value;
-    let texto = document.forms["form"]["comment"].value;
+    const nombre = document.forms["form"]["fname"].value;
+    const texto = document.forms["form"]["fcomment"].value;
 
     wrapper.className = "comentario";
+
+    let nombreDiv = document.createElement('p');
+    nombreDiv.className = "nombre";
+    nombreDiv.append(nombre, fechaDiv);
+
+    let heart = document.createElement('i');
+    heart.className = "fa-solid fa-heart animate__heartBeat";
+
     const textbox = document.createElement('div');
     textbox.innerHTML = texto;
-    document.forms["form"]["comment"].value = ""; // Borramos lo que haya escrito el usuario
+    textbox.className = "texto";
+    document.forms["form"]["fcomment"].value = ""; // Borramos lo que haya escrito el usuario
+
     // Añadimos los elementos (nombre, fecha, hora...)
-    wrapper.append(nombre, fecha, textbox);
+    wrapper.append(nombreDiv, textbox, heart);
 
     // Lo metemos en la sección de comentarios
     commentSection.appendChild(wrapper);
-}
-
-const commentContainer = document.getElementById('allComments');
-document.getElementById('addComments').addEventListener('click', function () {
-    addComment();
-});
-
-function addComment() {
-    let commentText;
-    const textBox = document.createElement('div');
-
-    const wrapDiv = document.createElement('div');
-    wrapDiv.className = 'wrapper';
-    wrapDiv.style.marginLeft = 0;
-    commentText = document.getElementById('newComment').value;
-    document.getElementById('newComment').value = '';
-    textBox.innerHTML = commentText;
-    wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
-    commentContainer.appendChild(wrapDiv);
-
 }
